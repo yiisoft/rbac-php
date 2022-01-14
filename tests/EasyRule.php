16a408e65078749f0ed83392563b49884f650766
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2022-01-14 16:59:42 +0300

    Adopt tests to `RuleInterface`

--- tests/EasyRule.php
+++ tests/EasyRule.php
@@ -4,12 +4,18 @@ declare(strict_types=1);
 
 namespace Yiisoft\Rbac\Php\Tests;
 
+use Yiisoft\Rbac\Item;
 use Yiisoft\Rbac\Rule;
 
-class EasyRule extends Rule
+final class EasyRule extends Rule
 {
     public function __construct()
     {
         parent::__construct(self::class);
+    }
+
+    public function execute(string $userId, Item $item, array $parameters = []): bool
+    {
+        return true;
     }
 }
