917512443e6fd61981864258b9c343415130d5f4
Alexey Rogachev <arogachev90@gmail.com>
2024-02-08 18:47:21 +0600

    Fix mutants

--- tests/ItemsStorageTest.php
+++ tests/ItemsStorageTest.php
@@ -19,6 +19,14 @@ final class ItemsStorageTest extends TestCase
     }
     use StorageFilePathTrait;
 
+    public $opcacheInvalidated = false;
+    private const EMPTY_STORAGE_TESTS = [
+        'testSaveWithNullAttributes',
+        'testLoadWithCustomGetFileUpdatedAt',
+        'testSaveAndInvalidateOpcacheWithExtension',
+        'testSaveAndInvalidateOpcacheWithoutExtension',
+    ];
+
     protected function setUp(): void
     {
         if ($this->name() === 'testFailCreateDirectory' || $this->name() === 'testCreateNestedDirectory') {
@@ -26,13 +34,56 @@ final class ItemsStorageTest extends TestCase
             FileHelper::clearDirectory($this->getTempDirectory());
         }
 
-        $this->traitSetUp();
+        if ($this->name() === 'testSaveAndInvalidateOpcacheWithExtension') {
+            uopz_set_return(
+                'function_exists',
+                static function (string $function): bool {
+                    return $function === 'opcache_invalidate' ? true : function_exists($function);
+                },
+                true,
+            );
+
+            $storage = $this;
+            uopz_set_return(
+                'opcache_invalidate',
+                static function (string $filename, bool $force = true) use ($storage): bool {
+                    $storage->opcacheInvalidated = true;
+
+                    return true;
+                },
+                true,
+            );
+        }
+
+        if ($this->name() === 'testSaveAndInvalidateOpcacheWithoutExtension') {
+            uopz_set_return(
+                'function_exists',
+                static function (string $function): bool {
+                    return $function === 'opcache_invalidate' ? false : function_exists($function);
+                },
+                true,
+            );
+        }
+
+        if (!in_array($this->name(), self::EMPTY_STORAGE_TESTS, strict: true)) {
+            $this->traitSetUp();
+        }
     }
 
     protected function tearDown(): void
     {
         if ($this->name() === 'testFailCreateDirectory' || $this->name() === 'testCreateNestedDirectory') {
             FileHelper::removeDirectory($this->getTempDirectory());
+        }
+
+        if ($this->name() === 'testSaveAndInvalidateOpcacheWithExtension') {
+            uopz_unset_return('function_exists');
+            uopz_unset_return('opcache_invalidate');
+            $this->opcacheInvalidated = false;
+        }
+
+        if ($this->name() === 'testSaveAndInvalidateOpcacheWithoutExtension') {
+            uopz_unset_return('function_exists');
         }
 
         $this->clearFixturesFiles();
@@ -59,6 +110,45 @@ final class ItemsStorageTest extends TestCase
         $storage->add(new Permission('createPost'));
 
         $this->assertFileExists($directory . '/items.php');
+    }
+
+    public function testSaveWithNullAttributes(): void
+    {
+        $this->createItemsStorage()->add(new Permission('test'));
+
+        $data = require $this->getDataPath() . DIRECTORY_SEPARATOR . 'items.php';
+        $this->assertSame([['name' => 'test', 'type' => 'permission']], $data);
+    }
+
+    public function testLoadWithCustomGetFileUpdatedAt(): void
+    {
+        $time = 1683707079;
+        $storage = new ItemsStorage(
+            $this->getDataPath(),
+            getFileUpdatedAt: static fn (string $filename): int|false => $time,
+        );
+        $storage->add(new Permission('test'));
+        $storage->load();
+
+        $this->assertSame($time, $storage->get('test')->getCreatedAt());
+    }
+
+    public function testSaveAndInvalidateOpcacheWithExtension(): void
+    {
+        $storage = $this->createItemsStorage();
+        $storage->add(new Permission('test'));
+
+        $this->assertCount(1, $storage->getAll());
+        $this->assertTrue($this->opcacheInvalidated);
+    }
+
+    public function testSaveAndInvalidateOpcacheWithoutExtension(): void
+    {
+        $storage = $this->createItemsStorage();
+        $storage->add(new Permission('test'));
+
+        $this->assertCount(1, $storage->getAll());
+        $this->assertFalse($this->opcacheInvalidated);
     }
 
     protected function createItemsStorage(): ItemsStorageInterface
