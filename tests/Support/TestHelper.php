3e2d64bc79e09f062b905a291620aa746be7d9d4
Sergei Predvoditelev <sergei@predvoditelev.ru>
2026-05-11 10:26:27 +0300

    Fix directory permission assertion in test to be umask-aware (#116)

--- tests/Support/TestHelper.php
+++ tests/Support/TestHelper.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 
 namespace Yiisoft\Rbac\Php\Tests\Support;
 
-use function sprintf;
-
 final class TestHelper
 {
     private function __construct() {}
@@ -15,10 +13,5 @@ final class TestHelper
         $currentHandler = set_error_handler(static fn() => true);
         restore_error_handler();
         return $currentHandler;
-    }
-
-    public static function getDirectoryPermissions(string $path): int
-    {
-        return octdec(substr(sprintf('%o', fileperms($path)), -4));
     }
 }
